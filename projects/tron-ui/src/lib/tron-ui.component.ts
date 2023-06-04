1bcd11e78846e01a78988aa85cafb13ba06fb741
Ahtritus <sutirthabharatidas@gmail.com>
2023-06-05 02:24:50 +0530

    ENH: Created a button component, exported tgz using pack

--- projects/tron-ui/src/lib/tron-ui.component.ts
+++ projects/tron-ui/src/lib/tron-ui.component.ts
@@ -1,20 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-tron-ui',
-  template: `
-    <p>
-      tron-ui works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <button [style.color]="color">{{ body }}</button> `,
+  styles: [],
 })
 export class TronUiComponent implements OnInit {
+  @Input() color: string = '#000';
+  @Input() body: string = 'Hello world';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
