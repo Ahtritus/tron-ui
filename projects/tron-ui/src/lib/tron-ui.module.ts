2d6b8c1337b7a0b95b6492eda1da552ff4564a0f
Sutirtha <sdas@republicfinance.com>
2023-06-11 22:16:22 +0530

    ENH: Created TronCardComponent

--- projects/tron-ui/src/lib/tron-ui.module.ts
+++ projects/tron-ui/src/lib/tron-ui.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { TronUiComponent } from './tron-ui.component';
+import { TronUiCardComponent } from './card/tron-card.component';
 
 
 
 @NgModule({
   declarations: [
-    TronUiComponent
+    TronUiComponent,
+    TronUiCardComponent
   ],
   imports: [
   ],
   exports: [
-    TronUiComponent
+    TronUiComponent,
+    TronUiCardComponent,
   ]
 })
 export class TronUiModule { }
