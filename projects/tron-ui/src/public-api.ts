6b277c55fe0c565bf60ff2ab705e7151ddb554ae
Sutirtha <sdas@republicfinance.com>
2023-06-13 13:49:23 +0530

    BLD: Enabled ivy, exporting all components in api

--- projects/tron-ui/src/public-api.ts
+++ projects/tron-ui/src/public-api.ts
@@ -4,5 +4,18 @@
 
 export * from './lib/tron-ui.service';
 export * from './lib/tron-ui.component';
-export * from './lib/tron-ui.module';
+export {TronUiModule} from './lib/tron-ui.module';
+
+export * from './lib/atoms/uni-tron-button/uni-tron-button.component';
+export * from './lib/atoms/uni-tron-input/uni-tron-input.component';
+export * from './lib/atoms/uni-tron-checkbox/uni-tron-checkbox.component';
+export * from './lib/atoms/uni-tron-radio-button/uni-tron-radio-button.component';
+
+export * from './lib/molecules/multi-tron-button-group/multi-tron-button-group.component';
+export * from './lib/molecules/multi-tron-toggle-switch/multi-tron-toggle-switch.component';
+
 export * from './lib/organisms/omni-tron-card/omni-tron-card.component';
+export * from './lib/organisms/omni-tron-sidebar/omni-tron-sidebar.component';
+
+export * from './lib/templates/super-tron-footer/super-tron-footer.component';
+export * from './lib/templates/super-tron-header/super-tron-header.component';
