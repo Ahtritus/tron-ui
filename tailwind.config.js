4cc123596b2b0a7fe9638fb7cbe89217a440e829
Sutirtha <sdas@republicfinance.com>
2023-06-21 12:43:55 +0530

    ENH: Using tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./src/**/*.{html,ts,css,scss}",
+  "./projects/tron-ui/src/**/*.{html,ts}",
+  "./node_modules/ui-components/**/*.{html,ts,js}"
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
